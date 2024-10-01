91bbcd93c9ab37e36157ad4272a5d0db28e27b41
saharalhabsi <sahar96alhabsi@gmail.com>
2024-10-02 01:23:56 +0400

    remove peft

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, send_file
 from transformers import AutoModel, AutoTokenizer
 import diffusers
 import torch
-from peft import PeftModel, PeftConfig
 from PIL import Image
 import os
 
