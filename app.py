98d60ed8bea8ad4598d869772f8b1d6a32d90cd6
saharalhabsi <sahar96alhabsi@gmail.com>
2024-10-02 02:12:09 +0400

    change proxy

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def generate():
     return send_file(image_path, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=19123)
+    app.run(debug=True, host='0.0.0.0', port=8080)
